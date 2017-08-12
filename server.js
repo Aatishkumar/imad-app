75495befb3ad79a3a3c38f11ac588ae3c5b445b7
Aatishkumar <mm15b001@smail.iitm.ac.in>
2017-08-12 23:16:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-// var Pool = require('pg').pool;
+var Pool = require('pg').Pool;
 
-// var config = {
-//   user: 'mm15b001',
-//   database: 'mm15b001',
-//   host: 'db-mm15b001-20949',
-//   port:'5432' ,
-//   password: process.env.DB_PASSWORD
-// };
+var config = {
+  user: 'mm15b001',
+  database: 'mm15b001',
+  host: 'db-mm15b001-20949',
+  port:'5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -18,18 +18,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-// var pool = new Pool (config);
-// app.get('/test-db', function(req, res){
-//     //make a select request
-//     //return a reponse with a results
-//     pool.query('SELECT * fROM test', function(err, result){
-//         if(err){
-//             res.status(500).send(err.toString());
-//         }else{
-//             res.send(JSON.stringfy(result));
-//         }
-//     });
-// });
+var pool = new Pool (config);
+app.get('/test-db', function(req, res){
+    //make a select request
+    //return a reponse with a results
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringfy(result));
+        }
+    });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
